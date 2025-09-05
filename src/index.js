b999d956acedd0fc8e952d18c32a4bb88a67cb15
aysunbekdemir <aysunkoc9@hotmail.com>
2025-09-05 22:03:03 +0300

    node2

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@
 require('dotenv').config(); // Ensure environment variables are loaded
 const { initMongoConnection } = require('./db/initMongoConnection');
 const setupServer = require('./server'); // Correctly import setupServer
+const Contact = require('./db/models/Contact');
 
 const startApp = async () => {
     await initMongoConnection();
     setupServer(); // Call the setupServer function
 };
 
+const checkContacts = async () => {
+    await initMongoConnection();
+    const contacts = await Contact.find();
+    if (contacts.length > 0) {
+        console.log('Contacts found:', contacts);
+    } else {
+        console.log('No contacts found in the collection.');
+    }
+};
+
 startApp();
+checkContacts();
